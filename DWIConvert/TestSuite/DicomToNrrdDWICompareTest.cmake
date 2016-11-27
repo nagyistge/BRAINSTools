088f0078b8f700f4353b1b515a7c4dfec892027b
Hans Johnson <hans-johnson@uiowa.edu>
2016-11-26 18:49:18 -0600

    ENH: Improve testing of FSL converted files.

--- DWIConvert/TestSuite/DicomToNrrdDWICompareTest.cmake
+++ DWIConvert/TestSuite/DicomToNrrdDWICompareTest.cmake
@@ -1,20 +1,27 @@
-
 # arguments checking
 if( NOT TEST_PROGRAM )
   message( FATAL_ERROR "Require TEST_PROGRAM to be defined" )
-endif( NOT TEST_PROGRAM )
+endif( )
+
 if( NOT TEST_COMPARE_PROGRAM )
   message( FATAL_ERROR "Require TEST_COMPARE_PROGRAM to be defined" )
-endif( NOT TEST_COMPARE_PROGRAM )
+endif( )
+
 if( NOT TEST_BASELINE )
   message( FATAL_ERROR "Require TEST_BASELINE to be defined" )
-endif( NOT TEST_BASELINE )
+endif( )
+
+if( NOT TEST_BASELINE_NHDR )
+  set( TEST_BASELINE_NHDR ${TEST_BASELINE} )
+endif( )
+
 if( NOT TEST_INPUT )
   message( FATAL_ERROR "Require TEST_INPUT to be defined" )
-endif( NOT TEST_INPUT )
+endif( )
+
 if( NOT TEST_TEMP_OUTPUT )
   message( FATAL_ERROR "Require TEST_TEMP_OUTPUT to be defined" )
-endif( NOT TEST_TEMP_OUTPUT )
+endif( )
 
 # Run the compare program to make sure it built correctly
 # execute_process(
@@ -25,12 +32,12 @@ endif( NOT TEST_TEMP_OUTPUT )
 # if the return value is !=0 bail out
 if( TEST_RESULT )
   message( FATAL_ERROR "Failed: Test compare program ${TEST_COMPARE_PROGRAM} won't run.\n${TEST_ERROR}" )
-endif( TEST_RESULT )
+endif( )
 
 # Check to see if the image we are comparing against exists.  We do this here to avoid a lengthy test for no reason.
 if(NOT EXISTS ${TEST_BASELINE})
   message( FATAL_ERROR "Failed: Baseline image ${TEST_BASELINE} does not exist!\n")
-endif( NOT EXISTS ${TEST_BASELINE})
+endif( )
 
 
 # run the test program, capture the stdout/stderr and the result var
@@ -49,7 +56,7 @@ if(TEST_FSL_FLAG)
   )
   if( TEST_RESULT )
     message( FATAL_ERROR "Failed: Test program ${TEST_PROGRAM} exited != 0.\n${TEST_ERROR}" )
-  endif( TEST_RESULT )
+  endif( )
 
 else()
   ## For testing purposes, convert from dicom to nhdr, nhdr to fsl, fsl to nrrd, then compare results
@@ -84,7 +91,7 @@ else()
   message(STATUS ${TEST_ERROR})
   if( TEST_RESULT )
     message( FATAL_ERROR "Failed: Test program ${Test_Command_Line_DICOMToNHDR} exited != 0.\n${TEST_ERROR}" )
-  endif( TEST_RESULT )
+  endif( )
 
   message(STATUS "\n\nTest_Command_Line_NHDRToFSL=${Test_Command_Line_NHDRToFSL}\n")
   execute_process(
@@ -95,7 +102,7 @@ else()
   message(STATUS ${TEST_ERROR})
   if( TEST_RESULT )
     message( FATAL_ERROR "Failed: Test program ${Test_Command_Line_NHDRToFSL} exited != 0.\n${TEST_ERROR}" )
-  endif( TEST_RESULT )
+  endif( )
 
   message(STATUS "\n\nTest_Command_Line_FSLToNrrd=${Test_Command_Line_FSLToNrrd}\n")
   execute_process(
@@ -106,73 +113,86 @@ else()
   message(STATUS ${TEST_ERROR})
   if( TEST_RESULT )
     message( FATAL_ERROR "Failed: Test program ${Test_Command_Line_FSLToNrrd} exited != 0.\n${TEST_ERROR}" )
-  endif( TEST_RESULT )
+  endif( )
 
 endif()
 
 if(TEST_FSL_FLAG)
-  if(NOT TEST_BASELINE_BVEC)
-    message( FATAL_ERROR "Require TEST_BASELINE_BVEC to be defined" )
-  endif()
-  if(NOT TEST_BASELINE_BVAL)
-    message( FATAL_ERROR "Require TEST_BASELINE_BVAL to be defined" )
-  endif()
-  if(NOT TEST_TEMP_BVEC)
-    message( FATAL_ERROR "Require TEST_TEMP_BVEC to be defined" )
-  endif()
-  if(NOT TEST_TEMP_BVAL)
-    message( FATAL_ERROR "Require TEST_TEMP_BVAL to be defined" )
-  endif()
+    if(NOT TEST_BASELINE_BVEC)
+      message( FATAL_ERROR "Require TEST_BASELINE_BVEC to be defined" )
+    endif()
+    if(NOT TEST_BASELINE_BVAL)
+      message( FATAL_ERROR "Require TEST_BASELINE_BVAL to be defined" )
+    endif()
+    if(NOT TEST_TEMP_BVEC)
+      message( FATAL_ERROR "Require TEST_TEMP_BVEC to be defined" )
+    endif()
+    if(NOT TEST_TEMP_BVAL)
+      message( FATAL_ERROR "Require TEST_TEMP_BVAL to be defined" )
+    endif()
 
-  if(NOT TEST_TEXT_COMPARE)
-    message( FATAL_ERROR "Require TEST_TEXT_COMPARE to be defined" )
-  endif()
-  set(Test_Compare_Command_Line
-    ${TEST_TEXT_COMPARE} ${TEST_BASELINE_BVEC} ${TEST_TEMP_BVEC}
-    ${TEST_BASELINE_BVAL} ${TEST_TEMP_BVAL})
-  message("Test_Compare_Command_Line=${Test_Compare_Command_Line}")
-  execute_process(COMMAND ${Test_Compare_Command_Line}
-    ERROR_VARIABLE TEST_ERROR
-    RESULT_VARIABLE TEST_RESULT)
+    if(NOT TEST_TEXT_COMPARE)
+      message( FATAL_ERROR "Require TEST_TEXT_COMPARE to be defined" )
+    endif()
+    set(Test_Compare_Command_Line_TEMP_OUTPUT
+      ${TEST_TEXT_COMPARE} ${TEST_BASELINE_BVEC} ${TEST_TEMP_BVEC}
+      ${TEST_BASELINE_BVAL} ${TEST_TEMP_BVAL})
+    message("Test_Compare_Command_Line_TEMP_OUTPUT=${Test_Compare_Command_Line_TEMP_OUTPUT}")
+    execute_process(COMMAND ${Test_Compare_Command_Line_TEMP_OUTPUT}
+      ERROR_VARIABLE TEST_ERROR
+      RESULT_VARIABLE TEST_RESULT)
+    if( TEST_RESULT )
+      message( FATAL_ERROR
+        "Failed: Test program ${TEST_TEXT_COMPARE} exited != 0.  ${TEST_ERROR}" )
+    endif( )
+  #   foreach(file VEC VAL)
+  #     file(READ ${TEST_BASELINE_B${file}} baseline)
+  #     file(READ ${TEST_TEMP_B${file}} testoutput)
+  #     if(testoutput STREQUAL baseline)
+  #       message("B${file} output matches")
+  #     else()
+  #       message(FATAL_ERROR "B${file} output doesn't match
+  # ${testoutput}
+  # ${baseline}")
+  #     endif()
+  #   endforeach()
+else()
+
+  # if the return value is !=0 bail out
   if( TEST_RESULT )
-    message( FATAL_ERROR
-      "Failed: Test program ${TEST_TEXT_COMPARE} exited != 0.
-${TEST_ERROR}" )
-  endif( TEST_RESULT )
-#   foreach(file VEC VAL)
-#     file(READ ${TEST_BASELINE_B${file}} baseline)
-#     file(READ ${TEST_TEMP_B${file}} testoutput)
-#     if(testoutput STREQUAL baseline)
-#       message("B${file} output matches")
-#     else()
-#       message(FATAL_ERROR "B${file} output doesn't match
-# ${testoutput}
-# ${baseline}")
-#     endif()
-#   endforeach()
+    message( FATAL_ERROR "Failed: Test program ${TEST_PROGRAM} exited != 0.\n${TEST_ERROR}" )
+  endif( )
+
+  #------------------------------------------------------------------------------------------
+  set(Test_Compare_Command_Line_TEMP_OUTPUT
+    ${TEST_COMPARE_PROGRAM} --inputVolume2 ${TEST_TEMP_OUTPUT} --inputVolume1 ${TEST_BASELINE}
+    )
+  message("Test_Compare_Command_Line_TEMP_OUTPUT=${Test_Compare_Command_Line_TEMP_OUTPUT}")
+  # now compare the output with the reference
+  execute_process(
+    COMMAND ${Test_Compare_Command_Line_TEMP_OUTPUT}
+    RESULT_VARIABLE TEST_RESULT
+    )
+  # again, if return value is !=0 scream and shout
+  if( TEST_RESULT )
+    message( FATAL_ERROR "Failed: ${Test_Compare_Command_Line_TEMP_OUTPUT}: ${TEST_RESULT}")
+  endif( )
+
+  #------------------------------------------------------------------------------------------
+  set(Test_Compare_Command_Line_NHDR_OUTPUT
+    ${TEST_COMPARE_PROGRAM} --inputVolume2 ${TEST_INITIAL_NHDR_OUTPUT} --inputVolume1 ${TEST_BASELINE_NHDR}
+    )
+  message("Test_Compare_Command_Line_NHDR_OUTPUT=${Test_Compare_Command_Line_NHDR_OUTPUT}")
+  # now compare the output with the reference
+  execute_process(
+    COMMAND ${Test_Compare_Command_Line_NHDR_OUTPUT}
+    RESULT_VARIABLE TEST_RESULT
+    )
+  # again, if return value is !=0 scream and shout
+  if( TEST_RESULT )
+    message( FATAL_ERROR "Failed: ${Test_Compare_Command_Line_NHDR_OUTPUT}: ${TEST_RESULT}")
+  endif( )
 endif()
 
-# if the return value is !=0 bail out
-if( TEST_RESULT )
-  message( FATAL_ERROR "Failed: Test program ${TEST_PROGRAM} exited != 0.\n${TEST_ERROR}" )
-endif( TEST_RESULT )
-
-set(Test_Compare_Command_Line
-  ${TEST_COMPARE_PROGRAM} --inputVolume2 ${TEST_TEMP_OUTPUT} --inputVolume1 ${TEST_BASELINE}
-  )
-message("Test_Compare_Command_Line=${Test_Compare_Command_Line}")
-# now compare the output with the reference
-execute_process(
-  COMMAND ${Test_Compare_Command_Line}
-  RESULT_VARIABLE TEST_RESULT
-  )
-# again, if return value is !=0 scream and shout
-if( TEST_RESULT )
-  message( FATAL_ERROR "Failed: The output of ${TEST_PROGRAM} did not match ${TEST_BASELINE}: ${TEST_RESULT}")
-endif( TEST_RESULT )
-
-
-
 # everything went fine...
-message( "Passed: The output of ${TEST_PROGRAM} matches ${TEST_BASELINE}" )
-
+message( "Passed: The output of ${TEST_PROGRAM} matches ${TEST_BASELINE}:${TEST_BASELINE_NHDR}" )
